84acfed38d261a3c9790becc91dd1047229ff6ff
Cassie Andrews <candrews1@ufl.edu>
2024-11-30 18:25:16 -0500

    guessLetter(letter) and updateScreen()

--- index.js
+++ index.js
@@ -26,16 +26,44 @@ class Word {
   }
 
   // implement the guessLetter function:
-  // guessLetter(letter) {}
+  guessLetter(letter) {
+    if (this.word.includes(letter) && !this.correctLetters.includes(letter)) { // if correct letter & not previously guessed
+ // *** SOLVE THIS ***     
+      this.displayWord = this.word.split('') // update displayWord to disp letter instead of _
+      this.correctLetters.push(letter) // add letter to correctLetters array
+    } else { // if incorrect letter
+      this.remainingGuesses -= 1 // decrement remainingGuesses by 1
+      this.incorrectLetters.push(letter) // add letter to incorrectLetters array
+    }
+  }
+
 
   // implement the updateScreen function:
-  // updateScreen() {}
+  // should update HTML with data from the object
+  updateScreen() {
+    // update #remaining-guesses with value in remainingGuesses
+    document.getElementById('remaining-guesses').textContent = this.remainingGuesses
+    // update #incorrect-letters with value in incorrectLetters
+    document.getElementById('incorrect-guess').textContent = this.incorrectLetters.join('')
+    // update #word-to-guess with value in displayWord
+    document.getElementById('word-to-guess').textContent = this.displayWord
+  }
 
   // implement the isGameOver function:
-  // isGameOver() {}
+  // should return true if game is over, false if game is not over
+  // default is false
+  isGameOver() {
+    // (true) game is over if:
+      // remainingGuesses <= 0
+      // if word === displayedWord
+  }
 
   // implement the getWinOrLoss function:
-  // getWinOrLoss() {}
+  // should return "win", "loss" when game is done or "null" if game is in progress
+  getWinOrLoss() {
+    // user has won if word === displayedWord AND remainingGuesses > 0
+    // user has lost if displayedWord is not equal to word AND remainingGuesses <= 0
+  }
 }
 
 function newGame() {
